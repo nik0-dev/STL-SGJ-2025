6c1cd7f442296c3d3330b9bc25a34b5779cd5ecc
Sophie Madi <sophieam701@gmail.com>
2025-05-24 16:21:17 -0500

    Basic setup

---
+++ Objects/Player/obj_player.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+# Follow cursor
+func process(delta):
+	
+	# Hide mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+	# Follow invisible cursor
+	position.x = get_global_mouse_position().x
+	position.y = get_global_mouse_position().y
+	

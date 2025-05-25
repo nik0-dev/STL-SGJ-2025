7170bf40a5b0dbc1ee44ff906895dc078b3bbecd
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2025-05-25 09:49:49 -0500

    remove redundant func

--- Scripts/battle_manager.gd
+++ Scripts/battle_manager.gd
@@ -4,12 +4,3 @@ static func get_opponent(player: Constants.BattlerType) -> Constants.BattlerType
 		Constants.BattlerType.Player: return Constants.BattlerType.Enemy
 		Constants.BattlerType.Enemy: return Constants.BattlerType.Player
 		_: return Constants.BattlerType.None
-
-static func flip_card(card: Card):
-	match card.side:
-		Constants.CardSide.Front: 
-			card.animation_player.play("flip_to_back")
-			card.side = Constants.CardSide.Back
-		Constants.CardSide.Back:
-			card.animation_player.play("flip_to_front")
-			card.side = Constants.CardSide.Front

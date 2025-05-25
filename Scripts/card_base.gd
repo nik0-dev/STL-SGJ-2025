7d4bebacc60882c3de54a759f8f8aa2c5deb1dd1
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2025-05-25 08:32:55 -0500

    card templating

--- Scripts/card_base.gd
+++ Scripts/card_base.gd
@@ -6,6 +6,7 @@ class_name CardBase extends Resource
 @export var image: Texture2D = Texture2D.new()
 @export var color: Color = Color.WHITE
 @export var tags: Array[String]
+@export var flavor_text: String
 
 @export_group("Battle")
 @export var target_type: Constants.TargetType = Constants.TargetType.None

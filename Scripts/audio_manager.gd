b0d9bcb0818d1cd6995216e4fb869a4dc158dd66
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2025-05-27 21:57:16 -0500

    adjusted audio channels

--- Scripts/audio_manager.gd
+++ Scripts/audio_manager.gd
@@ -1,11 +1,50 @@
-extends AudioStreamPlayer
+extends Node
 
-func play_audio(stream_to_play: AudioStream):
-	stream = stream_to_play
-	play()
+var active_channels: Array[AudioStreamPlayer] = [] 
+var inactive_channels: Array[AudioStreamPlayer] = []
 
-func play_audio_random_pitch(stream_to_play: AudioStream, scale_min: float, scale_max: float):
-	pitch_scale = randf_range(scale_min, scale_max) 
-	play_audio(stream_to_play)
-	await finished
-	pitch_scale = 1.0
+const POOLED_AUDIO_CHANNELS : int = 16
+
+func _ready() -> void:
+	for i in range(0, POOLED_AUDIO_CHANNELS):
+		instance_audio_channel()
+
+func instance_audio_channel():
+	var asp = AudioStreamPlayer.new()
+	var count : int = inactive_channels.size() + active_channels.size()
+	asp.name = "AudioStreamChannel" + str(count)
+	add_child(asp) 
+	inactive_channels.append(asp)
+	asp.finished.connect(swap_to_inactive.bind(asp))
+
+func swap_to_inactive(player: AudioStreamPlayer):
+	if active_channels.has(player):
+		active_channels.erase(player)
+		inactive_channels.append(player)
+
+func swap_to_active(player: AudioStreamPlayer):
+	if inactive_channels.has(player):
+		inactive_channels.erase(player)
+		active_channels.append(player)
+		
+func play_audio_impl(player: AudioStreamPlayer, stream: AudioStream):
+	if player != null && !player.playing:
+		player.stream = stream
+		swap_to_active(player)
+		player.play()
+	
+func play_audio(stream: AudioStream):
+	play_audio_impl(get_free_channel(), stream)
+
+func play_audio_random_pitch(stream: AudioStream, min: float, max: float):
+	var free_channel = get_free_channel()
+	if free_channel != null:
+		free_channel.pitch_scale = randf_range(min, max)
+		play_audio_impl(free_channel, stream)
+		await free_channel.finished
+		free_channel.pitch_scale = 1.0
+
+func get_free_channel() -> AudioStreamPlayer:
+	if inactive_channels.size() > 0:
+		return inactive_channels[0]
+	return null
